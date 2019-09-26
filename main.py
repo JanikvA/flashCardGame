d2d83da973a984719b9a71ff462acbccc22b7cb2
JanikvA <j.v.ahnen@gmx.de>
2019-09-26 10:22:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Simple game for learning flash cards.")
+    args = parser.parse_args()
+    main(args)
