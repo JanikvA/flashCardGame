c81540f1bd3dcc206ce7101890734ce8e34275af
Janik von Ahnen <j.v.ahnen@gmx.de>
2019-09-30 23:08:23 +0200

    scraped hsk1-3

--- scrapeFlashCards/scrape_DigMandarin.py
+++ scrapeFlashCards/scrape_DigMandarin.py
@@ -37,22 +37,24 @@ def parseDigmandaring(htmlText):
 
 
 def main(args):
-    outData={"flashCards":[]}
-    opener = AppURLopener()
-    response = opener.open('https://www.digmandarin.com/hsk-1-vocabulary-list.html')
-    html = response.read()
-    text = html.decode()
-    objects=parseDigmandaring(text)
-    for ob in objects:
-        outData["flashCards"].append({"Chinese":ob[0],"Pinyin":ob[1],"English":ob[2]})
-    with open('digmandarinFlashcards.json', 'w') as outfile:
-        json.dump(outData, outfile)
+    for wURL in args.website:
+        outData={"flashCards":[]}
+        opener = AppURLopener()
+        response = opener.open(wURL)
+        html = response.read()
+        text = html.decode()
+        objects=parseDigmandaring(text)
+        for ob in objects:
+            outData["flashCards"].append({"Chinese":ob[0],"Pinyin":ob[1],"English":ob[2]})
+        with open(wURL.split("/")[-1][:5].replace("-","_")+'_digmandarinFlashcards.json', 'w') as outfile:
+            json.dump(outData, outfile)
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(
         description="Scrape chinese data from https://www.digmandarin.com/hsk-1-vocabulary-list.html")
+    parser.add_argument("--website", default=None, nargs='*', help="website to be parsed",)
     parser.add_argument("-v", "--verbosity", help="increase output verbosity",
                         action="store_true", default=False)
     args = parser.parse_args()
