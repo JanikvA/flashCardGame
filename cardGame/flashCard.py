164073e041c9b6a537421f12634f057bc2900f5a
Janik von Ahnen <j.v.ahnen@gmx.de>
2019-10-02 21:07:13 +0200

    handling error if flash card is incomplete

--- cardGame/flashCard.py
+++ cardGame/flashCard.py
@@ -14,31 +14,37 @@ class flashCard:
     def flip(self, arg):
         pass
 
-    def showCardContent(self,show=None, dontShow=[]):
+    def showCardContent(self, show=None, dontShow=[]):
         prettyString = ""
         if show is None:
-           show=list(self.content.keys()) 
+            show = list(self.content.keys())
         for k in show:
             if k in dontShow:
                 continue
-            prettyString += str(k)+"  :  "+str(self.content[k])+"\n--------\n"
+            try:
+                prettyString += str(k)+"  :  "+str(self.content[k])+"\n--------\n"
+            except KeyError:
+                print("WARNING: {ky} not in this flash card".format(ky=k))
+                return self.showCardContent()
         return prettyString
 
     @classmethod
     def readFlashCardJSON(self, jsonName):
         flashCards = []
         if not os.path.isfile(jsonName):
-            print("WARNING: {jsonfile} does not exist!".format(jsonfile=jsonName))
+            print("WARNING: {jsonfile} does not exist!".format(
+                jsonfile=jsonName))
             return flashCards
         with open(jsonName) as json_file:
             try:
                 data = json.load(json_file)
             except json.decoder.JSONDecodeError:
-                print("WARNING: {jsonfile} can not be loaded! Wrong format?".format(jsonfile=jsonName))
+                print("WARNING: {jsonfile} can not be loaded! Wrong format?".format(
+                    jsonfile=jsonName))
                 return flashCards
             for cards in data["flashCards"]:
                 flashC = flashCard()
                 flashCards.append(flashC)
                 for side in cards:
-                    flashC.content[side]=cards[side]
+                    flashC.content[side] = cards[side]
         return flashCards
